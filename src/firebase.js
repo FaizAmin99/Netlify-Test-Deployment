9448db0fde9b2376c8bdf594948edfe899e805b5
Mohammad Faiz Amin <faizameen1199@gmail.com>
2023-06-13 16:01:13 +0500

    4:01PM
    change in firebase configration

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration, replace it with your project keys
 const firebaseConfig = {
-  apiKey: "apiKey",
-  authDomain: "authDomain",
-  projectId: "react-auth-tutorial-1234",
-  storageBucket: "storageBucket",
-  messagingSenderId: "messagingSenderId",
-  appId: "appId"
+  apiKey: "AIzaSyBAYOodwu1beXTbw0zjhnecjGykgIdQZro",
+  authDomain: "auth-2fc4e.firebaseapp.com",
+  databaseURL: "https://auth-2fc4e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "auth-2fc4e",
+  storageBucket: "auth-2fc4e.appspot.com",
+  messagingSenderId: "580593972277",
+  appId: "1:580593972277:web:c8a5041133138f0b9fad7c",
+  measurementId: "G-XYWTRWX3SQ"
 };
 
 // Initialize Firebase
